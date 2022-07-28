43955aea6e57d74913d7fedd0b7e93e4fc908929
Abhishek Mitra <AbMitra1999@gmail.com>
2022-07-27 23:57:10 -0700

    Making clean branch

--- memload.cpp
+++ memload.cpp
@@ -1,14 +1,9 @@
-#include <sw/redis++/redis++.h>
 #include <iostream>
 #include "happly.h"
 
-using namespace sw::redis;
 using namespace std;
 
 int main (int argc, char *argv[]) {
-  // Open redis interaction object
-  auto redis = Redis("tcp://127.0.0.1:6379");
-
   // Construct the data object by reading from file
   happly::PLYData plyIn("kinect_captures/1.ply");
 
@@ -16,22 +11,12 @@ int main (int argc, char *argv[]) {
   std::vector<std::array<double, 3>> vPos = plyIn.getVertexPositions();
   std::vector<std::array<unsigned char, 3>> vCol = plyIn.getVertexColors();
 
-  std::vector<std::array<double, 3>> vPosHead;
   int count = 10;
   for (auto x: vPos) {
     cout << x[0] << " " << x[1] << " " << x[2] << endl;
-    vPosHead.push_back(x);
     count --;
     if (count==0) {
       break;
     }
   }
-
-  redis.set("positions", vPosHead);
-  auto value = redis.get("positions");
-  if (value) {
-    std::vector<std::array<double, 3>> head = (std::vector<std::array<double, 3>>) (*value);
-    cout << head[0][0] << endl;
-  }
-  return 0;
 }
--- run_test.sh
+++
@@ -1,2 +0,0 @@
-g++ -std=c++17 -o redis memload.cpp /usr/local/lib/libredis++.a /usr/local/lib/libhiredis.a -pthread
-./redis
